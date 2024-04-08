eaa1a746ef56b3965a0026acdba1ca951b0f1afe
anushkamishra-1 <122478453+anushkamishra-1@users.noreply.github.com>
2024-04-08 17:06:54 +0530

    Add files via upload

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+
+
+
+    protected Rigidbody2D _rigidbody;
+
+    //using to initialize
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>(); 
+    }
+}
